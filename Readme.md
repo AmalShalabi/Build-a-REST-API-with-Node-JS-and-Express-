4ee44ac770f65a9daf84cbe435057202e5b02b4d
unknown <mouni.shalaby@gmail.com>
2022-09-03 13:32:43 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Build a REST API with Node JS and Express
+
+# TABLE OF CONTENT: 
+- Environment Variables
+- Route Parameters
+- Handling HTTP GET Requests
+- Handling HTTP POST Requests
+- Calling APIs using Postman
+- Handling HTTP DELETE Requests
---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import usersRoutes from './routes/users.js'
+
+const app=express();
+const port= 5000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes)
+app.get('/',(req,res)=> res.send('Hello from Hompage .'))
+
+app.listen(port,()=>{ console.log(`serever is running on port : http://localhost: ${port}`);})
